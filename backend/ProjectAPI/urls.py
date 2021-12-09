2d41fdaccd6080cbc954581971a7e7752ed3ef23
Ayush Gaikwad <22gaikwad.ayush@scsmustangs.org>
2021-12-09 16:02:29 -0500

    done

--- backend/ProjectAPI/urls.py
+++ backend/ProjectAPI/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls import url, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include('TeamSubmission.urls'))
+    
 ]
--- backend/TeamSubmission/urls.py
+++ backend/TeamSubmission/urls.py
@@ -4,5 +4,7 @@ from TeamSubmission import views
 
 urlpatterns=[
     url(r'^teamSubmission/$',views.teamSubmissionApi),
-    url(r'^teamSubmission/([0-9]+)$',views.teamSubmissionApi)
+    url(r'^teamSubmission/([0-9]+)$',views.teamSubmissionApi),
+    url(r'^Viewteams/$',views.teamSubmissionApi),
+    url(r'^Viewteams/([0-9]+)$',views.teamSubmissionApi)
 ]
